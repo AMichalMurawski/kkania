8b944c2e988c46f5f977d8f0de4fbb1c6059f942
Michał Murawski <m.a.murawski@wp.pl>
2025-09-15 16:14:50 +0200

    fix link reload site
    change tag a to Link from react-router-dom

--- src/elements/Links/Link.tsx
+++ src/elements/Links/Link.tsx
@@ -1,12 +1,28 @@
 import React, { PropsWithChildren } from "react";
 import styles from "./Link.module.css";
 import { LinkProps } from "./types";
+import { Link as RouterLink } from "react-router-dom";
 
-const Link: React.FC<PropsWithChildren<LinkProps>> = ({ children, href='', ariaLabel, anotherSite=false }) => {
+const Link: React.FC<PropsWithChildren<LinkProps>> = ({ children, href = '', ariaLabel, anotherSite = false }) => {
+    
+    if (anotherSite) {
+        return (
+            <a
+                href={href}
+                aria-label={ariaLabel}
+                className={styles.link}
+                target='_blank'
+                rel='noopener noreferrer'
+            >
+                {children}
+            </a>
+        );
+    };
+
     return (
-        <a href={href} aria-label={ariaLabel} className={styles.link} {...(anotherSite ? { target: '_blank', rel: 'noopener noreferrer'} : {})}>
+        <RouterLink to={href} aria-label={ariaLabel} className={styles.link}>
             {children}
-        </a>
+        </RouterLink>
     );
 };
 
