1b93673fb0807df296df299fbd4736675c3a253f
Michał Murawski <109108678+AMichalMurawski@users.noreply.github.com>
2025-09-22 23:59:52 +0200

    erase pwa

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles/fonts.css";
 import "./styles/index.css";
-import { registerSW } from 'virtual:pwa-register';
-
-registerSW({
-  onNeedRefresh() {
-    console.log("🔄 Nowa wersja dostępna");
-  },
-  onOfflineReady() {
-    console.log("✅ Aplikacja offline ready");
-  },
-});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
