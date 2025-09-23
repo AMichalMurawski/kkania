c62b18fcffb7cdb23de1cc7ae1b180fc92242c9a
Michał Murawski <m.a.murawski@wp.pl>
2025-09-23 15:46:27 +0200

    add hide header on mobile when is not top position and scrolling down

--- src/sections/Header/Header.tsx
+++ src/sections/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import styles from "./Header.module.css";
 import { Button, Hamburger, Logo } from "../../elements";
 import { Navbar } from "../../components";
@@ -8,8 +8,34 @@ import { useWindowWidth } from "../../hooks";
 const Header: FC = () => {
     const { data: routes } = useRoutes();
     const width = useWindowWidth();
+    const [scrollDirection, setScrollDirection] = useState<"up" | "down" | null>(null);
+    const [isAtTop, setIsAtTop] = useState(true);
 
-    return <header className={styles.header}>
+    useEffect(() => {
+        let lastScrollY = window.scrollY;
+
+        const updateScroll = () => {
+            const currentScrollY = window.scrollY;
+
+            if (currentScrollY > lastScrollY) {
+                setScrollDirection("down");
+            } else if (currentScrollY < lastScrollY) {
+                setScrollDirection("up");
+            }
+
+            setIsAtTop(currentScrollY === 0);
+
+            lastScrollY = currentScrollY;
+        };
+
+        window.addEventListener("scroll", updateScroll);
+
+        return () => {
+            window.removeEventListener("scroll", updateScroll);
+        };
+    }, []);
+
+    return <header className={`${styles.header} ${(width < 840 && scrollDirection === "down" && !isAtTop) ? styles.hide : null}`}>
         <div className={`container ${styles.headerContainer}`}>
             <Logo withoutText={width < 1200} homePath/>
             <div className={styles.navbarWrapper}>
