3ab61721921ed9b0d235557a72a05ed3315e2523
Michal M <m.a.murawski@wp.pl>
2025-09-30 08:20:12 +0200

    fix formType in ContactForm and ReservationForm

--- src/components/Forms/ContactForm/ContactForm.tsx
+++ src/components/Forms/ContactForm/ContactForm.tsx
@@ -11,99 +11,99 @@ import { sendEmail } from "../../../utils";
 type FormFields = keyof ContactFormData;
 
 const ContactForm: FC<ContactFormProps> = ({ darkStyle }) => {
-    const { addToast } = useToast();
+  const { addToast } = useToast();
 
-    const {
-        register,
-        handleSubmit,
-        formState: { errors, isSubmitting },
-        reset,
-        clearErrors,
-    } = useForm<ContactFormData>({
-        resolver: yupResolver(contactSchema),
-    });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+    clearErrors,
+  } = useForm<ContactFormData>({
+    resolver: yupResolver(contactSchema),
+  });
 
-    const handleFieldChange = (field: FormFields) => {
-        clearErrors(field);
-    };
+  const handleFieldChange = (field: FormFields) => {
+    clearErrors(field);
+  };
 
-    const onSubmit = async (data: ContactFormData) => {        
-        try {
-            addToast({
-                message: "Wysyłanie wiadomości",
-                type: "loading"
-            })
+  const onSubmit = async (data: ContactFormData) => {
+    try {
+      addToast({
+        message: "Wysyłanie wiadomości",
+        type: "loading",
+      });
 
-            const message = `
+      const message = `
                 <strong>Wiadomość:</strong> ${data.message}
-                <br>
                 <strong>Imię i Nazwisko:</strong> ${data.name}
-                <br>
                 <strong>E-mail:</strong> ${data.email}
             `;
 
-            const response = await sendEmail({
-                userName: data.name,
-                userEmail: data.email,
-                message: message,
-                formType: "kontakt"
-            });
+      const response = await sendEmail({
+        userName: data.name,
+        userEmail: data.email,
+        message: message,
+        formType: "kontakt",
+      });
 
-            if (!response.success) {
-                addToast({
-                    message: "Nastąpił problem z wysłaniem wiadomości",
-                    type: "error"
-                })
-                return
-            };
+      if (!response.success) {
+        addToast({
+          message: "Nastąpił problem z wysłaniem wiadomości",
+          type: "error",
+        });
+        return;
+      }
 
-            addToast({
-                message: "Wiadomość wysłana pomyślnie",
-                type: "emailSend"
-            })
-            reset();
-        } catch (err: any) {
-            console.log("Błąd wysyłki email:", err)
-            addToast({
-                message: "Wystąpił błąd przy wysyłaniu wiadomośći",
-                type: "error"
-            })
-        }
-    };
+      addToast({
+        message: "Wiadomość wysłana pomyślnie",
+        type: "emailSend",
+      });
+      reset();
+    } catch (err: any) {
+      console.log("Błąd wysyłki email:", err);
+      addToast({
+        message: "Wystąpił błąd przy wysyłaniu wiadomośći",
+        type: "error",
+      });
+    }
+  };
 
-    return (
-        <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-            <Input
-                darkStyle={darkStyle}
-                type="text"
-                label="Imię i Nazwisko:"
-                error={errors.name?.message}
-                {...register("name")}
-                onChange={() => handleFieldChange("name")}
-            />
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+      <Input
+        darkStyle={darkStyle}
+        type="text"
+        label="Imię i Nazwisko:"
+        error={errors.name?.message}
+        {...register("name")}
+        onChange={() => handleFieldChange("name")}
+      />
 
-            <Input
-                darkStyle={darkStyle}
-                type="email"
-                label="E-mail:"
-                error={errors.email?.message}
-                {...register("email")}
-                onChange={() => handleFieldChange("email")}
-            />
+      <Input
+        darkStyle={darkStyle}
+        type="email"
+        label="E-mail:"
+        error={errors.email?.message}
+        {...register("email")}
+        onChange={() => handleFieldChange("email")}
+      />
 
-            <Input
-                darkStyle={darkStyle}
-                type="textarea"
-                label="Wiadomość:"
-                placeholder="Napisz swoją wiadomość..."
-                error={errors.message?.message}
-                {...register("message")}
-                onChange={() => handleFieldChange("message")}
-            />
+      <Input
+        darkStyle={darkStyle}
+        type="textarea"
+        label="Wiadomość:"
+        placeholder="Napisz swoją wiadomość..."
+        error={errors.message?.message}
+        {...register("message")}
+        onChange={() => handleFieldChange("message")}
+      />
 
-            <Button type="submit" linkTo="" darkStyle disabled={isSubmitting}>Wyślij wiadomość</Button>
-        </form>
-    );
+      <Button type="submit" linkTo="" darkStyle disabled={isSubmitting}>
+        Wyślij wiadomość
+      </Button>
+    </form>
+  );
 };
 
 export default ContactForm;
--- src/components/Forms/ReservationForm/ReservationForm.tsx
+++ src/components/Forms/ReservationForm/ReservationForm.tsx
@@ -5,164 +5,163 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { Button, Input } from "../../../elements";
 import { reservationSchema } from "./reservationValidation";
 import { ReservationFormData, ReservationFormProps } from "./types";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { useOffers, useToast } from "../../../context";
 import { sendEmail } from "../../../utils";
 
 type FormFields = keyof ReservationFormData;
 
 const ReservationForm: FC<ReservationFormProps> = ({ darkStyle }) => {
-    const { data: offers } = useOffers();
-    const { addToast } = useToast();
-    const navigate = useNavigate();
-    const location = useLocation();
-    const [sessions, setSessions] = useState<{ value: string; label: string }[]>([]);
+  const { data: offers } = useOffers();
+  const { addToast } = useToast();
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [sessions, setSessions] = useState<{ value: string; label: string }[]>(
+    []
+  );
 
-    const {
-        register,
-        handleSubmit,
-        formState: { errors, isSubmitting },
-        reset,
-        setValue,
-        clearErrors,
-    } = useForm<ReservationFormData>({
-        resolver: yupResolver(reservationSchema),
-    });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+    setValue,
+    clearErrors,
+  } = useForm<ReservationFormData>({
+    resolver: yupResolver(reservationSchema),
+  });
 
-    useEffect(() => {
-        const mappedSessions = offers?.map((offer) => ({
-            value: offer.name,
-            label: offer.title,
-        }));
-        setSessions(mappedSessions || []);
-    }, [offers]);
+  useEffect(() => {
+    const mappedSessions = offers?.map((offer) => ({
+      value: offer.name,
+      label: offer.title,
+    }));
+    setSessions(mappedSessions || []);
+  }, [offers]);
 
-    useEffect(() => {
-        if (!sessions.length) return;
+  useEffect(() => {
+    if (!sessions.length) return;
 
-        const currentUrl = new URL(window.location.href);
-        const planFromUrl = currentUrl.searchParams.get("plan") || "";
+    const planFromUrl = searchParams.get("plan") || "";
 
-        setValue("session", planFromUrl);
-    }, [sessions, setValue, location]);
+    setValue("session", planFromUrl);
+  }, [sessions, setValue, searchParams]);
 
-    const handleSessionChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        const plan = e.target.value;
-        const currentUrl = new URL(window.location.href); 
-        currentUrl.searchParams.set("plan", plan);
-        const newPath = currentUrl.pathname + currentUrl.search
-        navigate(newPath, {replace: true});
-    };
+  const handleSessionChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const plan = e.target.value;
+    searchParams.set("plan", plan);
+    setSearchParams(searchParams, { replace: true });
+    // const currentUrl = new URL(window.location.href);
+    // currentUrl.searchParams.set("plan", plan);
+    // const newPath = currentUrl.pathname + currentUrl.search;
+    // navigate(newPath, { replace: true });
+  };
 
-    const handleFieldChange = (field: FormFields) => {
-        clearErrors(field);
-    };
+  const handleFieldChange = (field: FormFields) => {
+    clearErrors(field);
+  };
 
-    const onSubmit: SubmitHandler<ReservationFormData> = async (data) => {
-        try {
-            addToast({
-                message: "Wysyłanie wiadomości",
-                type: "loading"
-            })
-            
-            const message = `
+  const onSubmit: SubmitHandler<ReservationFormData> = async (data) => {
+    try {
+      addToast({
+        message: "Wysyłanie wiadomości",
+        type: "loading",
+      });
+
+      const message = `
                 <strong>Informacje o sesji:</strong> ${data.info}
-                <br>
                 <strong>Imię i nazwisko:</strong> ${data.name}
-                <br>
                 <strong>E-mail:</strong> ${data.email}
-                <br>
                 <strong>Sesja:</strong> ${data.session}
-                <br>
                 <strong>Data:</strong> ${data.date}
             `;
 
-            const response = await sendEmail({
-                userName: data.name,
-                userEmail: data.email,
-                message: message,
-                formType: "kontakt"
-            });
-            
-            if (!response.success) {
-                addToast({
-                    message: "Nastąpił problem z wysłaniem wiadomości",
-                    type: "error"
-                })
-                return
-            };
-        
-            addToast({
-                message: "Wiadomość wysłana pomyślnie",
-                type: "emailSend"
-            })
-            reset();
-        } catch (err: any) {
-            console.log("Błąd wysyłki email:", err)
-            addToast({
-                message: "Wystąpił błąd przy wysyłaniu wiadomośći",
-                type: "error"
-            })
-        }
-    };
+      const response = await sendEmail({
+        userName: data.name,
+        userEmail: data.email,
+        message: message,
+        formType: "rezerwacja",
+      });
 
-    return (
-        <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-            <Input
-                darkStyle={darkStyle}
-                type="text"
-                label="Imię i Nazwisko:"
-                error={errors.name?.message}
-                {...register("name")}
-                onChange={() => handleFieldChange("name")}
-            />
+      if (!response.success) {
+        addToast({
+          message: "Nastąpił problem z wysłaniem wiadomości",
+          type: "error",
+        });
+        return;
+      }
 
-            <Input
-                darkStyle={darkStyle}
-                type="email"
-                label="E-mail:"
-                error={errors.email?.message}
-                {...register("email")}
-                onChange={() => handleFieldChange("email")}
-            />
+      addToast({
+        message: "Wiadomość wysłana pomyślnie",
+        type: "emailSend",
+      });
+      reset();
+    } catch (err: any) {
+      console.log("Błąd wysyłki email:", err);
+      addToast({
+        message: "Wystąpił błąd przy wysyłaniu wiadomośći",
+        type: "error",
+      });
+    }
+  };
 
-            <Input
-                darkStyle={darkStyle}
-                type="select"
-                label="Sesja:"
-                options={sessions}
-                error={errors.session?.message}
-                {...register("session")}
-                onChange={(e) => {
-                    handleSessionChange(e);
-                    handleFieldChange("session");
-                }}
-            />
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+      <Input
+        darkStyle={darkStyle}
+        type="text"
+        label="Imię i Nazwisko:"
+        error={errors.name?.message}
+        {...register("name")}
+        onChange={() => handleFieldChange("name")}
+      />
 
-            <Input
-                darkStyle={darkStyle}
-                type="date"
-                label="Data:"
-                error={errors.date?.message}
-                {...register("date")}
-                onChange={() => handleFieldChange("date")}
-            />
+      <Input
+        darkStyle={darkStyle}
+        type="email"
+        label="E-mail:"
+        error={errors.email?.message}
+        {...register("email")}
+        onChange={() => handleFieldChange("email")}
+      />
 
-            <Input
-                darkStyle={darkStyle}
-                type="textarea"
-                label="Informacje o sesji:"
-                placeholder="Podaj dodatkowe informacje..."
-                error={errors.info?.message}
-                {...register("info")}
-                onChange={() => handleFieldChange("info")}
-            />
+      <Input
+        darkStyle={darkStyle}
+        type="select"
+        label="Sesja:"
+        options={sessions}
+        error={errors.session?.message}
+        {...register("session")}
+        onChange={(e) => {
+          handleSessionChange(e);
+          handleFieldChange("session");
+        }}
+      />
 
-            <Button type="submit" linkTo="" darkStyle disabled={isSubmitting}>
-                Wyślij wiadomość
-            </Button>
-        </form>
-    );
+      <Input
+        darkStyle={darkStyle}
+        type="date"
+        label="Data:"
+        error={errors.date?.message}
+        {...register("date")}
+        onChange={() => handleFieldChange("date")}
+      />
+
+      <Input
+        darkStyle={darkStyle}
+        type="textarea"
+        label="Informacje o sesji:"
+        placeholder="Podaj dodatkowe informacje..."
+        error={errors.info?.message}
+        {...register("info")}
+        onChange={() => handleFieldChange("info")}
+      />
+
+      <Button type="submit" linkTo="" darkStyle disabled={isSubmitting}>
+        Wyślij wiadomość
+      </Button>
+    </form>
+  );
 };
 
 export default ReservationForm;
