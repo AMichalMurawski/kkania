c716290683ac09f98ce8d9f062a3991fbbc51487
Michal M <m.a.murawski@wp.pl>
2025-10-23 22:21:38 +0200

    fix description view in OfferDescription -> each element - separate paragraph

--- src/components/OfferDescription/OfferDescription.tsx
+++ src/components/OfferDescription/OfferDescription.tsx
@@ -3,18 +3,32 @@ import styles from "./OfferDescription.module.css";
 import { OfferDescriptionProps } from "./types";
 import { OfferDetails } from "../";
 
-const OfferDescription: FC<OfferDescriptionProps> = ({offer, darkStyle}) => {
-    return (
-        <div className={`${styles.conteiner} ${darkStyle ? styles.dark : styles.light}`}>
-            <h2>{offer?.title}</h2>
-            <p>{offer?.description}</p>
-            <p className={styles.price}>{offer?.price} zł</p>
-            <p className={styles.listTitle}>Co obejmuje:</p>
-            <OfferDetails type={offer?.details.type || "list"} text={offer?.details.text || []} />
-            <p className={styles.listTitle}>Dlaczego warto?</p>
-            <OfferDetails type={offer?.whyChoose.type || "list"} text={offer?.whyChoose.text || []} />
-        </div>
-    );
+const OfferDescription: FC<OfferDescriptionProps> = ({ offer, darkStyle }) => {
+  return (
+    <div
+      className={`${styles.conteiner} ${
+        darkStyle ? styles.dark : styles.light
+      }`}
+    >
+      <h2>{offer?.title}</h2>
+      <>
+        {offer?.description.map((description, i) => (
+          <p key={i}>{description}</p>
+        ))}
+      </>
+      <p className={styles.price}>{offer?.price} zł</p>
+      <p className={styles.listTitle}>Co obejmuje:</p>
+      <OfferDetails
+        type={offer?.details.type || "list"}
+        text={offer?.details.text || []}
+      />
+      <p className={styles.listTitle}>Dlaczego warto?</p>
+      <OfferDetails
+        type={offer?.whyChoose.type || "list"}
+        text={offer?.whyChoose.text || []}
+      />
+    </div>
+  );
 };
 
 export default OfferDescription;
